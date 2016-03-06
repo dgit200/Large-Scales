e822f1f633c4042aebefa266be43d844a4ab5064
Dwight Thomas <dgit200@hotmail.com>
2016-03-05 20:36:44 -0500

    This is my Phone Directory Assignment

---
+++ Phone Directory/main/PhoneMain/Resources.java
@@ -0,0 +1,78 @@
+package PhoneMain;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class Resources 
+{//Figure out how to read and write properly in java and you will be good,
+//also comment every method in your classes so that you look professional 
+	public static String path = "PhoneDirectory.txt";
+	
+	public static int getLength()
+	{//This gets the number of entries that are stored in the file to help dynamically allocate space in memory for it.
+		int len = 0;
+		
+		try
+		{
+			String temp;
+			FileReader instream = new FileReader(Resources.path);
+			BufferedReader in = new BufferedReader(instream);
+			if((temp = in.readLine()) != null)
+			{
+				while((temp = in.readLine()) != null)
+				{
+					len++;
+				}
+			}
+			else
+			{
+				len = 0;
+			}
+			in.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error File Not Found");
+		}
+		return len;
+	}
+
+	public static void populateInMem(phone thisPhone)
+	{//This reads the directory's contents from the file into memory.
+		String Name, Num;
+		try
+		{
+			FileReader instream = new FileReader(Resources.path);
+			BufferedReader in = new BufferedReader(instream);
+			
+			while((Name = in.readLine()) != null)
+			{
+				Num = in.readLine();
+				thisPhone.addEntry(Name, Num);
+			}
+			in.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error File Not Found");
+		}
+	}
+
+	public static String getUserName()
+	{//This method is used to get the names from the user.
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the Name of the entry");
+		String name = reader.next();
+		return name;
+	}
+	
+	public static String getUserNum()
+	{//This method is used to get the numbers from the user.
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the number for the entry");
+		String num = reader.next();
+		return num;
+	}
+
+}
---
+++ Phone Directory/main/PhoneMain/phone.java
@@ -0,0 +1,126 @@
+package PhoneMain;
+
+import java.io.*;
+
+public class phone 
+{//This is the Declaration of the class phone which will hold the entries for the directory.
+	private	String [] num;
+	private String [] name;
+	private int current, lenght;
+	
+	public void construct(int len)
+	{//This is my constructor method it initializes my class variables
+	//It is assumed that You can only add 10 new directories at a time.
+		lenght = len + 10;
+		current = 0;
+		num = new String[lenght];
+		name = new String[lenght];
+	}
+	
+	public void addEntry(String fname, String number)
+	{//This add a new entry to the directory in memory.
+		name[current] = fname;
+		num[current] = number;
+		current++;
+		System.out.println("The entry was successfully entered");
+	}
+
+	public void delEntry(String pname)
+	{//This deletes a selected entry from the directory in memory.
+		int i = 0;
+		while(i < current)
+		{
+			if(pname.equals(name[i]))
+			{
+				for(int j=i; j< current; j++)
+				{
+					name[j] = name[j+1];
+					num[j] = num[j+1];
+				}
+				break;
+			}
+			i++;
+		}
+		if(i > current)
+		{
+			System.out.println("That Person was not found in the directory");
+		}
+		else
+		{
+			System.out.println("The entry was successfully deleted");
+		}
+	}
+
+	public String getNum(String pname)
+	{//This return the number of a particular name in the direcory or else it returns -1.
+		int i = 0;
+		while(i < current)
+		{
+			if(pname.equals(name[i]))
+			{
+				return num[i];
+			}
+			i++;
+		}
+		System.out.println("That Person was not found in the directory");
+		return "-1";
+	}
+
+	public void updateEntry(String pname, String number, String newName)
+	{//This updates the value of an entry.
+		int i = 0;
+		while(i <= current)
+		{
+			if(pname.equals(name[i]))
+			{
+				name[i] = newName;
+				num[i] = number;
+				break;
+			}
+			i++;
+		}
+		if(i > current)
+		{
+			System.out.println("That Person was not found in the directory");
+		}
+		else
+		{
+			System.out.println("The entry was successfully deleted");
+		}
+	}
+
+	public void writeToFile()
+	{//This function is used to write the directory from main memory back out to the file.
+		int i = 0;
+		try
+		{
+			FileWriter outstream = new FileWriter(Resources.path);
+			PrintWriter out = new PrintWriter(outstream);
+
+			while(i < current)
+			{
+				out.println(name[i]);
+				out.println(num[i]);
+				i++;
+			}
+			out.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("There Was and Error with the File");
+		}
+	}
+
+	public void printDirectory()
+	{//This method outputs the present contents in the directory to the screen.
+		int i = 0;
+		while(i < current)
+		{
+			System.out.println(name[i]);
+			System.out.println(num[i]);
+			i++;
+		}
+	}
+
+}
+
